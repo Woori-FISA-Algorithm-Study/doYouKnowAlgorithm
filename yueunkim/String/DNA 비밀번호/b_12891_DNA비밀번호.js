6dd77efdd501b53e3754dd1f710e154f66205a60
yueunkim <yueunkim0605@gmail.com>
2023-12-22 00:53:52 +0900

    add readme

--- yueunkim/String/DNA 비밀번호/b_12891_DNA비밀번호.js
+++ yueunkim/String/DNA 비밀번호/b_12891_DNA비밀번호.js
@@ -14,29 +14,28 @@ const dna = dnaStr.reduce((obj, key, index) => {
   return obj;
 }, {});
 let answer = 0;
-let strCount = { A: 0, C: 0, G: 0, T: 0 };
 
 // 1. index 0부터 부분문자열 개수만큼 substring만들기
 // 2. 각 문자 개수 세고 조건 만족하면 answer++
 // 3. index++하고 1번부터 반복
 
 // 시간초과
-// function solution(str, dna) {
-//     for (let i = 0; i < str.length - subStrLength; i++){
-//         let subStr = str.slice(i, i + Number(subStrLength)); // 부분문자열
+function solution(str, dna) {
+  for (let i = 0; i < str.length - subStrLength; i++) {
+    let subStr = str.slice(i, i + Number(subStrLength)); // 부분문자열
 
-//         // 각 문자 개수 세기
-//         let strCount = {A: 0, C: 0, G: 0, T: 0};
-//         for (let s of subStr) {
-//             if(s in dna) strCount[s]++;
-//         }
+    // 각 문자 개수 세기
+    let strCount = { A: 0, C: 0, G: 0, T: 0 };
+    for (let s of subStr) {
+      if (s in dna) strCount[s]++;
+    }
 
-//         // 조건 만족하면 answer++
-//         if (Object.keys(dna).every(key => dna[key] <= strCount[key])) {
-//             answer++;
-//         }
-//     }
-//     return answer;
-// }
+    // 조건 만족하면 answer++
+    if (Object.keys(dna).every((key) => dna[key] <= strCount[key])) {
+      answer++;
+    }
+  }
+  return answer;
+}
 
-// console.log(solution(str, dna));
+console.log(solution(str, dna));
