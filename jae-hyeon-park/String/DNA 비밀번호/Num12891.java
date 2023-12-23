6ddb0b65884c3828411d03002753de28790dbf94
Park Jae Hyeon <pgs1835@naver.com>
2023-12-23 14:22:31 +0900

    fail2

--- jae-hyeon-park/String/DNA 비밀번호/Num12891.java
+++ jae-hyeon-park/String/DNA 비밀번호/Num12891.java
@@ -4,12 +4,6 @@ import java.util.*;
 public class Num12891 {
     public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        //Scanner sc = new Scanner(System.in);
-        //String bf = sc.next();
-        //String bf = "4 2\n" + "GATA\n" + "1 0 0 1\n";
-
-
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
         int s = Integer.parseInt(st.nextToken());
@@ -17,64 +11,93 @@ public class Num12891 {
 
         char[] inValue = bf.readLine().toCharArray(); //들어온 값을 하나씩 넣기
 
-        int[] conditionAlpha =  {0, 0, 0, 0}; //필수 알파벳 개수 들어갈것 A,C,G,T 순
+        int[] conditionAlpha =  {0, 0, 0, 0}; //필수 알파벳 개수 들어갈것 A,C,G,T 순 1001
 
-        int count = 0; //acgt가 모두 원하는 값만큼 달성했는지 확인용
+        //int count = 0; //acgt가 모두 원하는 값만큼 달성했는지 확인용
+        
         int result = 0;
 
         st = new StringTokenizer(bf.readLine());
 
         for(int i=0;i<4;i++){
-            conditionAlpha[i] = Integer.parseInt(st.nextToken()); //필수 알파벳 개수 넣기
-            if(conditionAlpha[i]==0){
-                count++; //0인경우 미리 더해놓기
-            }
+            conditionAlpha[i] = Integer.parseInt(st.nextToken()); //필수 알파벳 개수 넣기           
         }
+        
+        int[] checkAlpha = {0,0,0,0}; //[비교할 배열
 
-        //int[] checkAlpha = {conditionAlpha[0],conditionAlpha[1],conditionAlpha[2],conditionAlpha[3]};
-
-
-        for(int i=0; i<=s-p; i++){
-            int[] checkAlpha = {conditionAlpha[0],conditionAlpha[1],conditionAlpha[2],conditionAlpha[3]}; //다시 조건 리셋
-
-            for(int j=i; j<p+i; j++){         //바뀌는 위치만큼 범위도 증가
-
-            switch (inValue[j]) {
-                case 'A':
-                    checkAlpha[0]--;
-                    if(checkAlpha[0] == 0){ //조건 달성시
-                        count++;
-                    }
-                case 'C':
-                    checkAlpha[1]--;
-                    if(checkAlpha[0] == 0){ //조건 달성시
-                        count++;
-                    }
-                case 'G':
-                    checkAlpha[2]--;
-                    if(checkAlpha[0] == 0){ //조건 달성시
-                        count++;
-                    }   
-                case 'T':
-                    checkAlpha[3]--;
-                    if(checkAlpha[0] == 0){ //조건 달성시
-                        count++;
-                    }    
-            
-                default:
-                    ;
-            }
-
-            if(count==4){ //모든 조건 달성
-                result++;
+        
+       for(int i=0; i<p; i++ ) {    	   
+    	   switch (inValue[i]) {
+           case 'A':
+               checkAlpha[0]++;             
+               break;
+               
+           case 'C':
+               checkAlpha[1]++;               
+               break;
+               
+           case 'G':
+               checkAlpha[2]++;                 
+               break;
+               
+           case 'T':
+               checkAlpha[3]++; 
+               break;
+       
+           default:
+               ;
+       }
+    	   if(checkAlpha[0]>=conditionAlpha[0]&& checkAlpha[1]>=conditionAlpha[1]&&checkAlpha[2]>=conditionAlpha[2]&&checkAlpha[3]>=conditionAlpha[3]){ //모든 조건 달성
+               result++;
+               break;
+           }
+       }  
+        
+        for(int i=1; i<=inValue.length-p; i++){ //첫 슬라이딩 윈도우  이후 마지막 슬라이딩 윈도우 까지 과정
+        	
+        	
+        	switch (inValue[i-1]) {
+            case 'A':
+                checkAlpha[0]--; 
                 break;
-            }
-                
-
+            case 'C':
+                checkAlpha[1]--;
+                break;
+            case 'G':
+                checkAlpha[2]--;
+                break;
+            case 'T':
+                checkAlpha[3]--;
+                break; 
+        
+            default:
+                ;
         }
+    
+        	switch (inValue[i+p-1]) {
+            case 'A':
+                checkAlpha[0]++;
+                break;
+            case 'C':
+                checkAlpha[1]++;
+                break;
+            case 'G':
+                checkAlpha[2]++;
+                break;  
+            case 'T':
+                checkAlpha[3]++;
+                break;    
+        
+            default:
+                ;
         }
 
+        	if(checkAlpha[0]>=conditionAlpha[0]&& checkAlpha[1]>=conditionAlpha[1]&&checkAlpha[2]>=conditionAlpha[2]&&checkAlpha[3]>=conditionAlpha[3]){ //모든 조건 달성
+                result++;             
+            }             
+        }
         System.out.println(result);
 
 	}
 }
+
