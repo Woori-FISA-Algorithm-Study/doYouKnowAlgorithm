2a1799e8da33d6a1e10c9e1e6fe5bd2fd8297474
LeeJincheon <pressky99@gmail.com>
2024-01-10 10:58:14 +0900

    3주차 추가

---
+++ LeeJincheon/4주차/동전 1/Main.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
+
+        int[] coin_value = new int[n];
+        for (int i=0; i<n; i++) {
+            coin_value[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[k+1];
+        // dp[j] = 가치의 합이 j원일 때의 경우의 수
+        for (int i=0; i<n; i++) {
+            for (int j=1; j<=k; j++) {
+                if (coin_value[i] == j) {
+                    dp[j]++;
+                } else if (coin_value[i] < j) {
+                    dp[j] += dp[j-coin_value[i]];
+                }
+            }
+        }
+
+        // idx: 0 1 2 3 4 5 6 7 8 9 10
+        // dp : 0 1 1 1 1 1 1 1 1 1 1
+        //          2 2 3 3 4 4 5 5 6
+        //                4 5 6 7 8 10
+
+        System.out.println(dp[k]);
+    } //main
+}
---
+++ LeeJincheon/4주차/포도주 시식/Main.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] wine = new int[n+1];
+
+        for (int i=1; i<=n; i++) {
+            wine[i] = Integer.parseInt(br.readLine());
+        }
+
+        if (n == 1) {
+            System.out.println(wine[1]);
+        } else if (n == 2) {
+            System.out.println(wine[1] + wine[2]);
+        } else {
+            int[] dp = new int[n+1];
+            dp[1] = wine[1];
+            dp[2] = wine[1] + wine[2];
+
+            int max = dp[2];
+
+            for (int i=3; i<=n; i++) {
+                dp[i] = Math.max(dp[i-1], Math.max(wine[i-1]+dp[i-3], dp[i-2])+wine[i]);
+                max = Math.max(max, dp[i]);
+            }
+
+            System.out.println(max);
+        }
+    } // main
+}
